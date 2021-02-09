b51b7d35f017034f40cbaa61236d09460d93616b
koki-pro <koki.banana03@gmail.com>
2021-02-09 17:19:45 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'top#index'
+
+  resources :list, only: [:new, :create, :edit, :update, :destroy] do
+    resources :card, except: [:index]
+  end
+end
